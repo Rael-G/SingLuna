5fa1e4837001e0f62b55612aa466c2bfd34071fd
Israel Albino Galvan <israel.galvan@live.com>
2024-10-30 16:56:39 -0300

    Cancelation token

--- SingLuna.Core/AudioService.cs
+++ SingLuna.Core/AudioService.cs
@@ -1,4 +1,3 @@
-using System;
 using CliWrap;
 using Discord.Audio;
 using YoutubeExplode;
@@ -10,21 +9,21 @@ internal static class AudioService
 {
     private static readonly YoutubeClient _youtubeClient = new();
 
-    public static async Task<VideoSearchResult> SearchYouTube(string query)
+    public static async Task<VideoSearchResult> SearchYouTube(string query, CancellationToken cancelationToken = default)
     {
-        var videos = _youtubeClient.Search.GetVideosAsync(query);
-        return await videos.FirstAsync();
+        var videos = _youtubeClient.Search.GetVideosAsync(query, cancelationToken);
+        return await videos.FirstAsync(cancelationToken);
     }
 
-    public static async Task<Stream> DownloadAudioAsync(string url)
+    public static async Task<Stream> DownloadAudioAsync(string url, CancellationToken cancelationToken = default)
     {
-        var streamManifest = await _youtubeClient.Videos.Streams.GetManifestAsync(url);
+        var streamManifest = await _youtubeClient.Videos.Streams.GetManifestAsync(url, cancelationToken);
         var audioStreamInfo = streamManifest.GetAudioOnlyStreams().First();
 
-        return await _youtubeClient.Videos.Streams.GetAsync(audioStreamInfo);
+        return await _youtubeClient.Videos.Streams.GetAsync(audioStreamInfo, cancelationToken);
     }
 
-    public static async Task SendAudioAsync(IAudioClient audioClient, Stream audioStream)
+    public static async Task SendAudioAsync(IAudioClient audioClient, Stream audioStream, CancellationToken cancelationToken = default)
     {
         var ffmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg", "ffmpeg.exe");
         MemoryStream ffmpegStream = new();
@@ -32,10 +31,10 @@ internal static class AudioService
             .WithArguments(" -hide_banner -loglevel panic -i pipe:0 -ac 2 -f s16le -ar 48000 pipe:1")
             .WithStandardInputPipe(PipeSource.FromStream(audioStream))
             .WithStandardOutputPipe(PipeTarget.ToStream(ffmpegStream))
-            .ExecuteAsync();
+            .ExecuteAsync(cancelationToken);
 
         using AudioOutStream discordStream = audioClient.CreatePCMStream(AudioApplication.Mixed);
-        try { await discordStream.WriteAsync(ffmpegStream.ToArray(), 0, (int)ffmpegStream.Length); }
-        finally { await discordStream.FlushAsync(); }
+        try { await discordStream.WriteAsync(ffmpegStream.ToArray(), 0, (int)ffmpegStream.Length, cancelationToken); }
+        finally { await discordStream.FlushAsync(cancelationToken); }
     }
 }
