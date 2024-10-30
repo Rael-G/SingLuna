ac1f45c805d85a107f33023fa45ccb604c1bcee9
Israel Albino Galvan <israel.galvan@live.com>
2024-10-30 14:47:05 -0300

    Converted program to Host

---
+++ SingLuna.Core/BotService.cs
@@ -0,0 +1,74 @@
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+
+namespace SingLuna.Core;
+
+public class BotService : IHostedService
+{
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+    private readonly IServiceProvider _services;
+
+    public BotService(DiscordSocketClient client, CommandService commands, IServiceProvider services)
+    {
+        _client = client;
+        _commands = commands;
+        _services = services;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+        _client.MessageReceived += HandleCommandAsync;
+
+        var token = Environment.GetEnvironmentVariable("DISCORD_API_TOKEN") ??
+                    throw new NullReferenceException("The Discord API token environment variable is not defined.");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await RegisterCommandsAsync();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private Task ReadyAsync()
+    {
+        Console.WriteLine($"Bot connected as {_client.CurrentUser}");
+        return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        var message = messageParam as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+
+        if (message == null || message.Author.IsBot) return;
+
+        int argPos = 0;
+        if (message.HasCharPrefix('/', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+        {
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                Console.WriteLine("Error: " + result.ErrorReason);
+        }
+    }
+}
--- SingLuna.Core/Program.cs
+++ SingLuna.Core/Program.cs
@@ -1,85 +1,29 @@
-﻿using System.Reflection;
-using Discord;
+﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SingLuna.Core;
 
 internal class Program
 {
-    private readonly DiscordSocketClient _client;
-    private readonly CommandService _commands;
-    private IServiceProvider _services;
-
-    public static async Task Main(string[] args) => await new Program().MainAsync();
-
-    public Program()
+    public static async Task Main(string[] args)
     {
-        _client = new DiscordSocketClient
-        (
-            new DiscordSocketConfig()
+        var host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
             {
-                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent,
-                LogLevel = LogSeverity.Info,
-            }
-        );
+                services.AddSingleton(provider => new DiscordSocketClient(new DiscordSocketConfig()
+                {
+                    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent,
+                    LogLevel = LogSeverity.Info,
+                }));
 
-        _commands = new CommandService();
-
-        _services = new ServiceCollection()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
-                .BuildServiceProvider();
-    }
-
-    private async Task MainAsync()
-    {
-        _client.Log += LogAsync;
-        _client.Ready += ReadyAsync;
-        _client.MessageReceived += HandleCommandAsync;
-
-        var token = Environment.GetEnvironmentVariable("DISCORD_API_TOKEN")??
-            throw new NullReferenceException("The discord api token is null");
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        await RegisterCommandsAsync();
-        
-        await Task.Delay(-1);
-    }
-
-    private Task LogAsync(LogMessage log)
-    {
-        Console.WriteLine(log);
-        return Task.CompletedTask;
-    }
-
-    private Task ReadyAsync()
-    {
-        Console.WriteLine($"Bot connected as {_client.CurrentUser}");
-        return Task.CompletedTask;
-    }
-
-    public async Task RegisterCommandsAsync()
-    {
-        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-    }
-
-    private async Task HandleCommandAsync(SocketMessage messageParam)
-    {
-        var message = messageParam as SocketUserMessage;
-        var context = new SocketCommandContext(_client, message);
-
-        
-        if (message == null || message.Author.IsBot) return;
-
-        int argPos = 0;
-        if (message.HasCharPrefix('/', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
-        {
-            Console.WriteLine($"Command received: {message?.Content}");
-            var result = await _commands.ExecuteAsync(context, argPos, _services);
-            if (!result.IsSuccess)
-                Console.WriteLine("Error: " + result.ErrorReason);
-        }
-    }
+                services.AddSingleton<CommandService>();
+                services.AddHostedService<BotService>();
+            });
 }
