0dc77c01778e183132d4849c2066cebeaf41293e
Israel Albino Galvan <israel.galvan@live.com>
2024-10-31 11:02:04 -0300

    Fixed skip with less responsability

--- SingLuna.Core/Playlist.cs
+++ SingLuna.Core/Playlist.cs
@@ -42,32 +42,28 @@ public class Playlist(IAudioClient client, ITextChannel channel, SocketGuild gui
 
     public async Task Skip()
     {
-        if (!_queue.TryDequeue(out var first))
-            return;
-
         if (_actual is not null)
         {
             await ReplyAsync($"Skiping \"{_actual.Video.Title}\"");
             _actual.Stop();
-
         }
-        _actual = first;
-        await ReplyAsync($"Playing \"{_actual.Video.Title}\"");
-        await _actual.Play(_client, OnMusicEnd);
     }
 
     public async Task Stop()
     {
         await ReplyAsync("Stopping");
         _actual?.Stop();
-        await _client.StopAsync();
+        try { await _client.StopAsync(); }
+        catch(OperationCanceledException e)
+        { Console.WriteLine(e.Message); }
+        finally
+        { PlaylistEnded(_guild); }
     }
 
     public async Task EndPlaylist()
     {
         await ReplyAsync("There is no more musics on the playlist");
         await Stop();
-        PlaylistEnded(_guild);
     }
 
     private async void OnMusicEnd()
